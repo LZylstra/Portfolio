60e97c8cb4acc0db5b80dd4c277ea170f577dbd5
LZylstra <lindseyzylstra@gmail.com>
2019-10-06 18:35:44 -0700

    working on store of projects

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-STORE[];
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+function loadProjects(){
+
+$('.project-slideshow').html(`
+<div class = "mySlides fade"></div>
+<img src="img/QuizAppPic2.png">
+<div class = "project-text">
+  <h3>Halloween Quiz App</h3>
+  <h4>Technology used:</h4>
+  <ul class = "tech">
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>jQuery</li>
+    <li>Javascript</li>
+  </ul>
+  <h4>Brief description of project:</h4>
+  <p>Halloween trivia quiz with 5 multiple choice questions</p>
+  <a id = "github" href = "https://github.com/LZylstra/Quiz-App"><i class="fab fa-github-square"></i> GitHub</a>
+  <a href = "https://lzylstra.github.io/Quiz-App/">Live Demo </a>
+</div>
+</div>  `);
+}
+
---
+++ projects.js
@@ -0,0 +1,23 @@
+const PROJECTS = [
+    {id: 0, 
+        project: "Future Project",
+        techused: [],
+        description: "This space will be updated with new projects soon!",
+        github: "",
+        live: ""
+    },  
+    {id: 1, 
+        project: "Halloween Quiz App",
+        techused: ["HTML", "CSS", "jQuery", "JavaScript"],
+        description: "Halloween trivia quiz with 5 multiple choice questions",
+        github: "https://github.com/LZylstra/Quiz-App",
+        live: "https://lzylstra.github.io/Quiz-App/"
+    },
+    {id: 2, 
+        project: "Azazel's Revenge",
+        techused: ["Written in C"],
+        description: "2D platformer game and game engine with physics and graphics",
+        github: "https://github.com/LZylstra/Azazels-Revenge",
+        live: ""
+    }
+];
