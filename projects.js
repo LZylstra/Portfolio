165451e76a1a72cd6eb22368c574a20679d5cb25
LZylstra <lindseyzylstra@gmail.com>
2020-04-23 18:00:22 -0700

    updates to projects

--- projects.js
+++ projects.js
@@ -1,23 +1,62 @@
 const PROJECTS = [
-    {id: 0, 
-        project: "Future Project",
-        techused: [],
-        description: "This space will be updated with new projects soon!",
-        github: "",
-        live: ""
-    },  
-    {id: 1, 
-        project: "Halloween Quiz App",
-        techused: ["HTML", "CSS", "jQuery", "JavaScript"],
-        description: "Halloween trivia quiz with 5 multiple choice questions",
-        github: "https://github.com/LZylstra/Quiz-App",
-        live: "https://lzylstra.github.io/Quiz-App/"
-    },
-    {id: 2, 
-        project: "Azazel's Revenge",
-        techused: ["Written in C"],
-        description: "2D platformer game and game engine with physics and graphics",
-        github: "https://github.com/LZylstra/Azazels-Revenge",
-        live: ""
-    }
+  {
+    id: 0,
+    project: "Tattoo Task Tracker",
+    techused: [
+      "React.js",
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Node.js",
+      "Express",
+      "PostgreSQL",
+    ],
+    description:
+      "An application targeted at tattoo artists that helps them stay organized by giving them the ability to create and view schedules as well as manage client and tattoo information.",
+    clientgithub: "https://github.com/LZylstra/Tattoo-Planner-app",
+    apigithub: "https://github.com/LZylstra/Tattoo-Planner-api",
+    live: "https://tattoo-planner-app.now.sh/",
+  },
+  {
+    id: 1,
+    project: "Project Tracker",
+    techused: [
+      "React.js",
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Node.js",
+      "Express",
+      "PostgreSQL",
+    ],
+    description:
+      "An application targeted at companies to help them manage group projects. Allows users to track to do lists for each project as well as priority. Provides the ability to assign tasks from the to do list to employees of the company. Admin role available with the ability to mass delete projects or tasks.",
+    clientgithub: "https://github.com/LZylstra/project-tracker-client",
+    apigithub: "https://github.com/LZylstra/project-tracker-api",
+    live: "https://project-tracker.now.sh/",
+  },
+  {
+    id: 2,
+    project: "Help Me Decide",
+    techused: [
+      "HTML",
+      "CSS",
+      "jQuery",
+      "Javascript",
+      "Yelp Fusion API",
+      "Edamam API",
+    ],
+    description:
+      "Our app provides helpful suggestions when you’re hungry and are not sure what to eat. You can choose to receive suggestions on something to cook at home or for restaurants near you.",
+    github: "https://github.com/LZylstra/Help-Me-Decide",
+    live: "https://lzylstra.github.io/Help-Me-Decide/",
+  },
+  {
+    id: 3,
+    project: "Halloween Quiz App",
+    techused: ["HTML", "CSS", "jQuery", "JavaScript"],
+    description: "Halloween trivia quiz with 5 multiple choice questions",
+    github: "https://github.com/LZylstra/Quiz-App",
+    live: "https://lzylstra.github.io/Quiz-App/",
+  },
 ];
